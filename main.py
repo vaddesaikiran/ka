d862b80d1a9c189479aeb6a3065f58fe11ffb9bc
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-11-15 15:33:28 +0530

    added basic kafka with updates

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
+import time
 import requests
 from quixstreams import Application
 import json
+import logging
 
 # Make a request to the Open-Meteo API
-response = requests.get("https://api.open-meteo.com/v1/forecast",
-                params={
-                        "latitude": 51.5,
-                        "longitude": -0.11,
-                        "current_weather": "true",  # Corrected API parameter
-                    },
+def get_weather():
+    response = requests.get("https://api.open-meteo.com/v1/forecast",
+        params={
+                "latitude": 51.5,
+                "longitude": -0.11,
+                "current_weather": "true",  # Corrected API parameter
+            },
         )
 
 # Get the weather data as JSON
-weather = response.json()
+    return response.json()
 
 # Create a QuixStreams Application instance
-app = Application(
-    broker_address="localhost:9092",
-    loglevel="DEBUG",  # Fixed missing comma
-)
+def main():
+        app = Application(
+            broker_address="localhost:9092",
+            loglevel="DEBUG",  # Fixed missing comma
+        )
 
-# Produce the weather data to Kafka
- # This will print the formatted weather data
-with app.get_producer() as producer:
-    producer.produce(
-        topic="weather_data_demo",  # Corrected typo in the topic name
-        key="London",
-        value=json.dumps(weather),
-    )
+        # Produce the weather data to Kafka
+        with app.get_producer() as producer:
+            while True:
+                    weather = get_weather()
+                    logging.debug("Got Weather: %s", weather)
+                    producer.produce(
+                        topic="weather_data_demo",  # Corrected typo in the topic name
+                        key="London",
+                        value=json.dumps(weather),
+                    )
+                    logging.info("produced...sleeping")
+                    time.sleep(10)
+
+if __name__=="__main__":
+    logging.basicConfig(level="DEBUG")
+    main()
